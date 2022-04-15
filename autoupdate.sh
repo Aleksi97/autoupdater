a7963a46326352ba486bb5d45b42dea011da0ea5
Aleksi Raivio <aleksi.raivio@outlook.com>
2022-04-15 08:30:45 +0300

    initial commit

---
+++ autoupdate.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+LOG_FILE="/var/log/cron/autoupdate.log"
+DATETIME_STAMP=$(date +"%d.%m.%Y | %H:%M:%S")
+
+PREFIX="---${DATETIME_STAMP}---"
+SUFFIX="---------------------------"
+
+DISTRO=$1
+
+function Debian() {
+	echo $PREFIX >> ${LOG_FILE}
+	apt-get -q update && apt-get -q upgrade -y >> ${LOG_FILE}
+	echo $SUFFIX >> ${LOG_FILE}
+}
+
+function Rhel() {
+	echo $PREFIX >> ${LOG_FILE}
+	dnf -q update -y >> ${LOG_FILE}
+	echo $SUFFIX >> ${LOG_FILE}
+}
+
+if [[ "${DISTRO}" == "-d" ]] ; then
+	Debian
+elif [[ "${DISTRO}" == "-r" ]] ; then
+	Rhel
+else
+	echo $PREFIX >> ${LOG_FILE}
+	echo "Error: Invalid distro parameter" >> ${LOG_FILE}
+	echo $SUFFIX >> ${LOG_FILE}
+	exit 1
+fi
