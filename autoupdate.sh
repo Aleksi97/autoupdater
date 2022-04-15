d31cb7636cd99c5e983c8e9741d676058c865a3d
Aleksi Raivio <aleksi.raivio@outlook.com>
2022-04-15 09:12:09 +0300

    Add linebreaks to logging

--- autoupdate.sh
+++ autoupdate.sh
@@ -3,21 +3,21 @@
 LOG_FILE="/var/log/cron/autoupdate.log"
 DATETIME_STAMP=$(date +"%d.%m.%Y | %H:%M:%S")
 
-PREFIX="---${DATETIME_STAMP}---"
-SUFFIX="---------------------------"
+PREFIX="---------------------------\n---${DATETIME_STAMP}---"
+SUFFIX="---------------------------\n"
 
 DISTRO=$1
 
 function Debian() {
-	echo $PREFIX >> ${LOG_FILE}
+	echo -e $PREFIX >> ${LOG_FILE}
 	apt-get -q update && apt-get -q upgrade -y >> ${LOG_FILE}
-	echo $SUFFIX >> ${LOG_FILE}
+	echo -e $SUFFIX >> ${LOG_FILE}
 }
 
 function Rhel() {
-	echo $PREFIX >> ${LOG_FILE}
+	echo -e $PREFIX >> ${LOG_FILE}
 	dnf update -y >> ${LOG_FILE}
-	echo $SUFFIX >> ${LOG_FILE}
+	echo -e $SUFFIX >> ${LOG_FILE}
 }
 
 if [[ "${DISTRO}" == "-d" ]] ; then
@@ -25,8 +25,8 @@ if [[ "${DISTRO}" == "-d" ]] ; then
 elif [[ "${DISTRO}" == "-r" ]] ; then
 	Rhel
 else
-	echo $PREFIX >> ${LOG_FILE}
+	echo -e $PREFIX >> ${LOG_FILE}
 	echo "Error: Invalid distro parameter" >> ${LOG_FILE}
-	echo $SUFFIX >> ${LOG_FILE}
+	echo -e $SUFFIX >> ${LOG_FILE}
 	exit 1
 fi
