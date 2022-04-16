b23e2bb39ad84c9d244f65d3f168172d0d9156ba
Aleksi Raivio <aleksi.raivio@outlook.com>
2022-04-16 18:37:01 +0300

    Split logdir and logfile, Add error func, shorten if statements

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-LOG_FILE="/var/log/cron/autoupdate.log"
+LOG_DIR="/var/log/cron/"
+LOG_FILE="autoupdate.log"
 DATETIME_STAMP=$(date +"%d.%m.%Y | %H:%M:%S")
 
 PREFIX="---------------------------\n---${DATETIME_STAMP}---"
@@ -9,24 +10,28 @@ SUFFIX="---------------------------\n"
 DISTRO=$1
 
 function Debian() {
-	echo -e $PREFIX >> ${LOG_FILE}
-	apt-get -q update && apt-get -q upgrade -y >> ${LOG_FILE}
-	echo -e $SUFFIX >> ${LOG_FILE}
+	echo -e $PREFIX >> ${LOG_DIR}${LOG_FILE}
+	apt-get -q update && apt-get -q upgrade -y >> ${LOG_DIR}${LOG_FILE}
+	echo -e $SUFFIX >> ${LOG_DIR}${LOG_FILE}
+	exit 1
 }
 
 function Rhel() {
-	echo -e $PREFIX >> ${LOG_FILE}
-	dnf update -y >> ${LOG_FILE}
-	echo -e $SUFFIX >> ${LOG_FILE}
+	echo -e $PREFIX >> ${LOG_DIR}${LOG_FILE}
+	dnf update -y >> ${LOG_DIR}${LOG_FILE}
+	echo -e $SUFFIX >> ${LOG_DIR}${LOG_FILE}
+	exit 1
 }
 
-if [[ -n "$DISTRO" ]] && [[ "${DISTRO}" == "-d" ]] ; then
-	Debian
-elif [[ -n "$DISTRO" ]] && [[ "${DISTRO}" == "-r" ]] ; then
-	Rhel
-else
-	echo -e $PREFIX >> ${LOG_FILE}
-	echo "Error: Invalid distro parameter" >> ${LOG_FILE}
-	echo -e $SUFFIX >> ${LOG_FILE}
+function Error() {
+	echo -e $PREFIX >> ${LOG_DIR}${LOG_FILE}
+	echo "Error: Invalid distro parameter" >> ${LOG_DIR}${LOG_FILE}
+	echo -e $SUFFIX >> ${LOG_DIR}${LOG_FILE}
 	exit 1
-fi
+}
+
+[[ -d ${LOG_DIR} ]] && mkdir ${LOG_DIR} && touch ${LOG_DIR}${LOG_FILE}
+
+[[ -n "$DISTRO" ]] && [[ "${DISTRO}" == "-d" ]] && Debian
+[[ -n "$DISTRO" ]] && [[ "${DISTRO}" == "-r" ]] && Rhel
+Error
